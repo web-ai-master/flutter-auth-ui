983a77bbc448fb2834624ce146c4209ec1a4c67c
jovan0901 <jovanvelickovic0901@gmail.com>
2021-08-07 20:20:13 +0530

    :sparkles: Create constant.dart file

---
+++ lib/constant.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+//Colors
+const BackgroundColor = Color(0xff191720);
+const TextFileFill = Color(0xff1E1C24);
+
+//TextStyles
+const HEadline = TextStyle(
+  color: Colors.white,
+  fontSize: 34,
+  fontWeight: FontWeight.bold
+);
+
+const BodyText = TextStyle(
+  color: Colors.grey,
+  fontSize: 15,
+);
+
+const ButtonText = TextStyle(
+  color: Colors.black87,
+  fontSize: 16,
+  fontWeight: FontWeight.bold
+);
+
+const BodyText2 = TextStyle(
+  fontSize:25,
+  fontWeight:FontWeight.w500,
+  color: Colors.white
+);
+
