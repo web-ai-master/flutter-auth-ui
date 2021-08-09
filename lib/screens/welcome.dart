c035703ead5100ed25ef9f0eadb9f850953eaec0
jovan0901 <jovanvelickovic0901@gmail.com>
2021-08-09 17:07:44 +0530

    :bug: Fix define onTap void callBack error

--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -8,86 +8,76 @@ class WelcomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            children: [
-              Flexible(
-                child: Column(
-                  children: [
-                    Center(
-                      child: Container(
-                        width: MediaQuery.of(context).size.width * 0.8,
-                        child: Image(
-                          image: AssetImage('assets/images/team_illustration.png'),
-                        ),
-                      ),
+          child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            Flexible(
+              child: Column(children: [
+                Center(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    child: Image(
+                      image: AssetImage('assets/images/team_illustration.png'),
                     ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      "Enterprise team\ncollaboration.",
-                      style: Headline,
-                      textAlign: TextAlign.center
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.8,
-                      child: Text(
-                        "Bring together your files, your tools, project and people, including a new mobile and desktop application.",
-                        style: BodyText,
-                        textAlign: TextAlign.center,
-                      ),
-                    )
-                  ]
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom:15.0),
-                child: Container(
-                  height: 60,
-                  decoration: BoxDecoration( 
-                    color: Colors.grey[850],
-                    borderRadius: BorderRadius.circular(20)
                   ),
-                  child: Row(
-                    children: [
-                       Expanded(
-                        child: MyTextButton(
-                          bgColor: Colors.white,
-                          buttonName: 'Register',
-                          onTap: () {
-                            
-                          },
-                          textColor: Colors.black87,
-                        ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text("Enterprise team\ncollaboration.",
+                    style: Headline, textAlign: TextAlign.center),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  child: Text(
+                    "Bring together your files, your tools, project and people, including a new mobile and desktop application.",
+                    style: BodyText,
+                    textAlign: TextAlign.center,
+                  ),
+                )
+              ]),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 15.0),
+              child: Container(
+                height: 60,
+                decoration: BoxDecoration(
+                    color: Colors.grey[850],
+                    borderRadius: BorderRadius.circular(20)),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: MyTextButton(
+                        bgColor: Colors.white,
+                        buttonName: 'Register',
+                        onTap: () {},
+                        textColor: Colors.black87,
                       ),
-                      Expanded(
-                        child: MyTextButton(
-                          bgColor: Colors.transparent,
-                          buttonName: 'Sign In',
-                          onTap: () {
-                            Navigator.push(
+                    ),
+                    Expanded(
+                      child: MyTextButton(
+                        bgColor: Colors.transparent,
+                        buttonName: 'Sign In',
+                        onTap: () {
+                          Navigator.push(
                               context,
                               CupertinoPageRoute(
                                 builder: (context) => Login(),
-                              )
-                            );
-                          },
-                          textColor: Colors.white,
-                        ),
+                              ));
+                        },
+                        textColor: Colors.white,
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
-        )
-      ),
+            ),
+          ],
+        ),
+      )),
     );
   }
 }
@@ -101,7 +91,7 @@ class MyTextButton extends StatelessWidget {
     required this.textColor,
   }) : super(key: key);
   final String buttonName;
-  final Function onTap;
+  final VoidCallback onTap;
   final Color bgColor;
   final Color textColor;
 
@@ -120,7 +110,7 @@ class MyTextButton extends StatelessWidget {
             (states) => Colors.black12,
           ),
         ),
-        onPressed: () => onTap,
+        onPressed: onTap,
         child: Text(
           buttonName,
           style: ButtonText.copyWith(color: textColor),
